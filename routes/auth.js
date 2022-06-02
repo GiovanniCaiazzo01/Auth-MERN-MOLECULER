1030534a6bad826fe2076e1d3e84fa80b0bb8cc8
GiovanniCaiazzo01 <giovannicaiazzo@gmail.com>
2022-06-02 21:45:02 +0200

    update login

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,11 @@ const router = express.Router();
 
 router.post("/register", async (req, res) => {
   const { username, email, password } = req.body;
+  if (!email || !password)
+    console.log({
+      result: false,
+      message: "Please Provide all the data in the form ",
+    });
   const data = await global.broker.call("auth.register", {
     username,
     email,
@@ -13,11 +18,27 @@ router.post("/register", async (req, res) => {
 
 router.get("/login", async (req, res) => {
   const { username, email, password } = req.body;
+  if (!email || !password)
+    console.log({
+      result: false,
+      message: "Please Provide all the data in the form ",
+    });
 
+  const check_password = await global.db.collection("Users").findOne(password);
   const check_email = await global.db.collection("Users").findOne(email);
-  if (!check_email) return { result: false, message: "" };
+  if (!check_email || !check_password) {
+    return {
+      result: false,
+      message: "The E-mail or password provided are not correct ",
+    };
+  }
+  const data = await global.broker.call("auth.login", {
+    username,
+    email,
+    password,
+  });
 
-  res.send(login);
+  res.send(data);
 });
 
 router.get("/forgotpassword", async (req, res) => {
@@ -29,4 +50,5 @@ router.get("/resetpassword", async (req, res) => {
   const rest_pwd = await global.broker.call("auth.resetpassword");
   res.send(rest_pwd);
 });
+
 module.exports = router;
