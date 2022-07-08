f0d44b99dde0b9f779c35ee351577b7390df919c
GiovanniCaiazzo01 <giovannicaiazzo@gmail.com>
2022-07-08 19:07:10 +0200

    levato il provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,22 @@
 import "./app.css";
 import { Routes, Route } from "react-router-dom";
-import { useState, useContext } from "react";
+import { useState, useEffect, useContext } from "react";
 
 import Login from "./components/Login/Login";
 import Home from "./components/Home/Home";
 import PrivateRoute from "./components/PrivateRoute/PrivateRoute";
-import UserContextProvider from "./components/provider/UserContextProvider";
 
 const App = () => {
-  const [userToken, setUserToken] = useState();
-  const [userData, setUserData] = useState({});
-
-  const retriveData = (data) => {
-    setUserData(data);
-  };
+  const [userData, setUserData] = useState();
 
   return (
     <div className="app">
-      <UserContextProvider value={userData}>
-        <Routes>
-          <Route element={<PrivateRoute />}>
-            <Route path="/" element={<Home />} exact />
-          </Route>
-
-          <Route path="/login" element={<Login retriveData={retriveData} />} />
-        </Routes>
-      </UserContextProvider>
+      <Routes>
+        <Route element={<PrivateRoute />}>
+          <Route path="/" element={<Home />} exact />
+        </Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
     </div>
   );
 };
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,26 +1,27 @@
 import React, { useState } from "react";
 import { Row, Col, Card, Input, Space, Button } from "antd";
 import Messages from "../pieces/Messages/Messages";
-
 import axios from "axios";
 import "antd/dist/antd.css";
 import "./login.css";
 
-const Login = ({ retriveData }) => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
   const [result, setResult] = useState(false);
-  const [token, setToken] = useState("");
+  const [message, setMessage] = useState("");
 
-  const data = {
-    email,
-    password,
-    message,
-    result,
-    token,
+  const handleLogin = async () => {
+    const data = await axios.post("http://127.0.0.1:5000/auth/login", {
+      email,
+      password,
+    });
+
+    const res = data.data;
+
+    setResult(res.result);
+    setMessage(res.message);
   };
-  retriveData(data);
 
   return (
     <Row
@@ -45,8 +46,7 @@ const Login = ({ retriveData }) => {
                 defaultValue="1"
                 addonBefore="Password"
               />
-              <Button onClick={retriveData}>Login</Button>
-
+              <Button onClick={handleLogin}>Login</Button>
               {result === false ? (
                 <Messages type={"error"} message={message} />
               ) : (
