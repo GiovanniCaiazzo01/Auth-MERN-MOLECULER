88474d02a71542e989d02777e391095b0b3a3d55
GiovanniCaiazzo01 <giovannicaiazzo002@gmail.com>
2022-08-11 10:46:34 +0200

    inizializzato progetto senza CRA

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,7 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MainRoutes from "./routes";
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <MainRoutes />
-    </BrowserRouter>
-  );
+  return <div>Hello scem</div>;
 };
 
 export default App;
--- client/src/components/PrivateRoute/PrivateRoute.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useEffect } from "react";
-import { Outlet, Navigate } from "react-router-dom";
-
-const PrivateRoute = ({ children, token }) => {
-  return token ? children : <Navigate to="/login" />;
-};
-
-export default PrivateRoute;
